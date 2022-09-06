afc311ecf54c9ae7e76be2b19d30560395244aef
BUZZ <seungjin429@gmail.com>
2022-09-06 15:34:44 +0900

    - date U&I UI 완성

---
+++ date_u_and_i/android/app/src/main/kotlin/com/example/date_u_and_i/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.date_u_and_i
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ date_u_and_i/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:date_u_and_i/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
